ae28963772055f2a6fac1a114c9fd3cdd57c6273
lqs <lv.qingsong@foxmail.com>
2023-09-23 12:39:06 +0800

    Update pretrain.py

--- pretrain.py
+++ pretrain.py
@@ -8,8 +8,6 @@ import torch.nn as nn
 import torch
 import argparse
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '7,6,5'
-os.environ['TOKENIZERS_PARALLELISM'] = 'true'
 
 
 class Trainset(Dataset):
@@ -23,7 +21,7 @@ class Trainset(Dataset):
                     sents.append(sent)
 
         input_ids, target_ids, mask = [], [], []
-        res = tokenizer(sents)
+        res = tokenizer(sents,truncation=True,max_length=256)
         input_ids = res['input_ids']
         mask = res['attention_mask']
         target_ids = []
@@ -59,16 +57,15 @@ def collate_fn(batch):
     return batch_input_ids, batch_target_ids, batch_mask
 
 
-def train_epoch(epoch, model, dataloader, creterion, optimizer):
+def train_epoch(epoch, model, dataloader, creterion, optimizer, print_interval):
     local_rank = int(os.environ['LOCAL_RANK'])
     master=(local_rank==0)
     if master:
         print(f'Epoch {epoch}:')
-        # 1000个batch输出一次
-        _print_interval = 1000
+        # 100个batch输出一次
         n_batch = len(dataloader)
-        cur_batch_loss = 0
         stt0 = time.time()
+        cur_batch_loss = 0
         stt1 = time.time()
     
     sampler.set_epoch(epoch)
@@ -82,7 +79,7 @@ def train_epoch(epoch, model, dataloader, creterion, optimizer):
         loss = creterion(logits.transpose(1, 2), target_ids)
         loss = loss*mask
         loss = loss.sum()/loss.shape[0]
-
+        # print(loss)
         if master:
             cur_batch_loss += loss.cpu().item()
 
@@ -90,13 +87,13 @@ def train_epoch(epoch, model, dataloader, creterion, optimizer):
         loss.backward()
         optimizer.step()
 
-        if master and (batch_idx+1) % _print_interval == 0:
+        if master and batch_idx % print_interval == 0:
             print(f'\t{batch_idx}th batch - {batch_idx}/{n_batch} ({(batch_idx)/n_batch*100:.1f}%)')
-            print(f'\t\tcurrent batch loss: {cur_batch_loss}, cost {time.time()-stt1} seconds.')
+            print(f'\t\tcurrent batch loss: {cur_batch_loss:.2f}, cost {time.time()-stt1:.1f} seconds.')
             cur_batch_loss = 0
             stt1=time.time()
     if local_rank==0:
-        print(f'Epoch {epoch} cost {time.time()-stt0} seconds.\n')
+        print(f'Epoch {epoch} cost {time.time()-stt0:.1f} seconds.\n')
 
 
 if __name__ == "__main__":
@@ -109,11 +106,12 @@ if __name__ == "__main__":
     n_gpus = torch.cuda.device_count()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-sentence_path', default='./data/sentences.txt')
-    parser.add_argument('-model_save_path', default='./model')
-    parser.add_argument('-n_epoch', default=4, type=int)
-    parser.add_argument('-lr', default=5e-5, type=float)
-    parser.add_argument('-batch_size', default=10, type=int)
+    parser.add_argument('--sentence_path', default='./data/sentences.txt')
+    parser.add_argument('--model_save_path', default='./model')
+    parser.add_argument('--n_epoch', default=4, type=int)
+    parser.add_argument('--lr', default=5e-5, type=float)
+    parser.add_argument('--batch_size', default=20, type=int)
+    parser.add_argument('--print_interval', default=100, type=int)
     args = parser.parse_args()
 
     sentence_path = args.sentence_path
@@ -121,6 +119,7 @@ if __name__ == "__main__":
     n_epoch = args.n_epoch
     lr = args.lr
     batch_size = args.batch_size
+    print_interval = args.print_interval
 
     if not os.path.exists(model_save_path):
         os.mkdir(model_save_path)
@@ -128,33 +127,38 @@ if __name__ == "__main__":
     # model_name="bigscience/bloom-3b"
     model_name = '/vepfs/lvqs/local_transformers/bloom-3b'
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name).bfloat16()
+    model.train()
     model.cuda()
+    for name,param in model.named_parameters():
+        param.requires_grad=True
     ddp_model = DDP(model)
 
-    creterion = nn.CrossEntropyLoss(reduction='none')
+    creterion = nn.CrossEntropyLoss(reduction='none',label_smoothing=0.1)
     optimizer = AdamW(ddp_model.parameters(), lr=lr)
 
-    print(f'local_rank:{local_rank} begins to build dataset.\n')
+    print(f'local_rank:{local_rank} begins to build dataset.')
     trainset = Trainset(sentence_path, tokenizer)
-    print(f'local_rank:{local_rank} ends to build dataset.\n')
+    print(f'local_rank:{local_rank} ends to build dataset.')
+    dist.barrier()
+
     sampler = DistributedSampler(trainset)
     trainloader = DataLoader(
         trainset, batch_size, collate_fn=collate_fn, num_workers=4, sampler=sampler)
 
     if master:
-        print('Begin to train model.\n')
+        print('\nBegin to train model.\n')
         stt=time.time()
     
     for epoch in range(1, n_epoch+1):
         train_epoch(epoch, model=ddp_model, dataloader=trainloader,
-                    creterion=creterion, optimizer=optimizer)
+                    creterion=creterion, optimizer=optimizer, print_interval=print_interval)
         if master:
             _save_path = os.path.join(model_save_path, f'epoch_{epoch}.pth')
             torch.save(model.state_dict(), _save_path)
 
     if master:
         print('End to train model.')
-        print(f'Totally cost {time.time()-stt} seconds.\n')
+        print(f'Totally cost {time.time()-stt:.1f} seconds.\n')
 
     dist.destroy_process_group()
